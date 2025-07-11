2dda6d765e750832ac2460f52adbe2908f91349b
Noel Rivero <noel.rivero@lacoloniamedical.com>
2025-07-11 16:01:21 -0400

    chore: delete the default app module

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { UsersModule } from './users/users.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(UsersModule);
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('User Authentication Example API')
