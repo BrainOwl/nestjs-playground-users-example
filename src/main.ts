0a99f289a94077567adcc539ce44a7550c73a488
Noel Rivero <noel.rivero@lacoloniamedical.com>
2025-07-11 16:16:20 -0400

    feat: add app module to centralize all modules

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { UsersModule } from './users/users.module';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(UsersModule);
+  const app = await NestFactory.create(AppModule);
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('User Authentication Example API')
