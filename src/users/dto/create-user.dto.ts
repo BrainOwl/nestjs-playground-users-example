49baf66fd9179b7742e99b7993bd0cdfceef3520
Noel Rivero <noel.rivero@lacoloniamedical.com>
2025-07-11 15:43:30 -0400

    feat: update create method

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Role } from '@prisma/client';
+
+export class CreateUserDto {
+  @ApiProperty({ required: true })
+  email: string;
+
+  @ApiProperty({ required: false })
+  telephone: string;
+
+  @ApiProperty({ required: true })
+  firstName: string;
+
+  @ApiProperty({ required: false })
+  lastName: string;
+
+  @ApiProperty({ required: true })
+  password: string;
+
+  @ApiProperty({ required: false })
+  role?: Role = 'USER';
+}
